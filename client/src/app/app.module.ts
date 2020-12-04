8bf33bb9c71aa3eb9f419ff751ca196a9ad4bbe7
owen-j-i-n <71049419+owen-j-i-n@users.noreply.github.com>
2020-12-04 00:41:27 -0500

    Update app.module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -29,7 +29,12 @@ import { NzSwitchModule } from 'ng-zorro-antd/switch';
 import { NzTagModule } from 'ng-zorro-antd/tag';
 import { NavComponent } from './nav/nav.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { registerLocaleData } from '@angular/common';
+import en from "@angular/common/locales/en"
+registerLocaleData(en)
 
+import { NZ_I18N,en_US } from 'ng-zorro-antd/i18n'
+ 
 @NgModule({
   declarations: [
     AppComponent,
@@ -66,7 +71,7 @@ import { UserListComponent } from './user-list/user-list.component';
     NzSwitchModule,
     NzTagModule
   ],
-  providers: [NzAnchorComponent],
+  providers: [NzAnchorComponent,{provide:NZ_I18N,useValue:en_US}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
