7eb0802e8b4e28639ca15f2d00f225cec49b6ca7
owen-j-i-n <71049419+owen-j-i-n@users.noreply.github.com>
2020-12-04 23:07:20 -0500

    Update auth.js

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,6 +1,6 @@
 //to authanticate 
 module.exports = function (req, res, next) {
-    // if (req.isAuthenticated()) return next()
+
     req.passport && req.passport.authenticate('jwt', { session: false }, (err, user, info) => {
       if (err) { return next(err) }
       
